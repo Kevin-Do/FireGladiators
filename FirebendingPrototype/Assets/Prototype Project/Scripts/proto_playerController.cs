fb738e7c512c35b872d18fd9f9ef5e24aa208dec
Kevin Do <kevindokhoale@gmail.com>
2017-12-10 17:43:51 -0800

    Synced Bullets and Player Identification
    [+] [Command]
    [+] NetworkServer Spawn

--- FirebendingPrototype/Assets/Prototype Project/Scripts/proto_playerController.cs
+++ FirebendingPrototype/Assets/Prototype Project/Scripts/proto_playerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityStandardAssets.Effects;
 
 public class proto_playerController : NetworkBehaviour
 {
@@ -11,10 +12,16 @@ public class proto_playerController : NetworkBehaviour
 	public float _playerJumpForce;
 
 	public float _playerRollDistance;
-	// Use this for initialization
+
+	public GameObject bulletprefab;
+
+	public Transform bulletSpawn;
+
+	public float bulletSpeed;
+	
 	public override void OnStartLocalPlayer ()
 	{
-		GetComponent<Material>().color = Color.cyan;
+		GetComponent<Renderer>().material.color = Color.cyan;
 	}
 	
 	// Update is called once per frame
@@ -24,10 +31,30 @@ public class proto_playerController : NetworkBehaviour
 		{
 			return;
 		}
+		
 		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
 		var z = Input.GetAxis("Vertical") * Time.deltaTime * _playerSpeed;
 
+		if (Input.GetKeyDown("space"))
+		{
+			CmdFire();
+		}
+		
 		transform.Rotate(0, x, 0);
 		transform.Translate(0, 0, z);
+	}
+
+	[Command]
+	void CmdFire()
+	{
+		var bullet = (GameObject) Instantiate(
+			bulletprefab,
+			bulletSpawn.position,
+			bulletSpawn.rotation);
+		bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
+
+		//Spawn on clients
+		NetworkServer.Spawn(bullet);
+		Destroy(bullet, 2.0f);
 	}
 }
